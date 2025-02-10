3c6267474794699386ab2460f7f44aa7661e8210
Saurabh Rai <51364763+saurabh-raiiii@users.noreply.github.com>
2025-02-10 09:18:36 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContextProvider = (props) => {
 
     axios.defaults.withCredentials = true;
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const backendUrl = 'https://mern-auth-nzzy.onrender.com';
     const [isLoggedin, setIsLoggedin] = useState(false);
     const [userData, setUserData] = useState(false)
 
